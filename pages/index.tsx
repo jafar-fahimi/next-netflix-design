3acb63916618d050d4976340dde39e21ee28187d
jafar <jafar.fahimi12@gmail.com>
2023-01-30 15:58:29 +0430

    <Row /> imported in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,11 +41,13 @@ const Home = ({
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
         <section>
-          <Row />
-          {/* row */}
-          {/* row */}
-          {/* row */}
-          {/* row */}
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
