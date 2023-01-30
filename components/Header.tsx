801b47bff4e915314dd6054f198a7a2f8e1af106
jafar <jafar.fahimi12@gmail.com>
2023-01-30 09:39:27 +0430

    Netfilx logo added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,13 @@
+import { SearchIcon, BellIcon } from '@heroicons/react/solid'
 import React from 'react'
+import Link from 'next/link'
 
 function Header() {
   return (
     <header>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
-          src="https://rb.gy/ulxxee"
+          src="/Netflix_2015_logo.svg"
           width={100}
           height={100}
           className="cursor-pointer object-contain"
@@ -20,6 +22,18 @@ function Header() {
           <li className="headerLink">New & Popular</li>
           <li className="headerLink">My List</li>
         </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
       </div>
     </header>
   )
