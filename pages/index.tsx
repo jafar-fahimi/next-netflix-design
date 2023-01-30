36500e867b705d83a07fe2513b53f5e1d961851f
jafar <jafar.fahimi12@gmail.com>
2023-01-30 09:19:01 +0430

    index.tsx intial blueprint of page writed

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+import React from 'react'
+
+// function Home() : NextPage = {
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Home - Netflix Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      bulild
+      {/* Header */}
+      <main>
+        {/* <Banner /> */}
+        <section>
+          {/* row */}
+          {/* row */}
+          {/* row */}
+          {/* row */}
+        </section>
+      </main>
+      {/* Modal */}
+    </div>
+  )
+}
+export default Home
