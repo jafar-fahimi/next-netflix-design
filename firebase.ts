ae0234dcb270aeb32e50e93a6c3932dad8eb5620
jafar <jafar.fahimi12@gmail.com>
2023-02-18 20:50:56 +0430

    firebase.ts created

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+};
+
+// Initialize Firebase // always know why! orelse your memorizing!
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+console.log("getApps().length: ", getApps().length);
+const db = getFirestore();
+const auth = getAuth();
+
+export default app;
+export { auth, db };
