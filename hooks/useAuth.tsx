2ccd3a646f442bf9f3179d350dc8ae361fcd8a87
jafar <jafar.fahimi12@gmail.com>
2023-02-19 11:36:27 +0430

    hooks/useAuth.tsx signUp created

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -11,5 +11,17 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { auth } from "../firebase";
 
 export default function UseAuth() {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  
+  const signUp = (email: string, password: string) => {
+    setLoading(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+      })
+      .catch((err) => alert(err.message))
+      .finally(() => setLoading(false));
+  };
   return;
 }
