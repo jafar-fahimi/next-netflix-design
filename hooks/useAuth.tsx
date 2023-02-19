578aac3aaf29f3a59c492329e36b1eb56e864fc8
jafar <jafar.fahimi12@gmail.com>
2023-02-19 11:42:29 +0430

    hooks/useAuth.tsx logout created

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -6,30 +6,41 @@ import {
   User,
 } from "firebase/auth";
 
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { auth } from "../firebase";
 
 export default function UseAuth() {
   const [loading, setLoading] = useState(false);
   const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
 
   const signUp = (email: string, password: string) => {
     setLoading(true);
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         setUser(userCredential.user);
+        router.push("/");
       })
       .catch((err) => alert(err.message))
       .finally(() => setLoading(false));
   };
-  
+
   const signIn = (email: string, password: string) => {
     setLoading(true);
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         setUser(userCredential.user);
+        router.push("/");
       })
+      .catch((err) => alert(err.message))
+      .finally(() => setLoading(false));
+  };
+
+  const logout = () => {
+    setLoading(true);
+    signOut(auth)
+      .then(() => setUser(null))
       .catch((err) => alert(err.message))
       .finally(() => setLoading(false));
   };
