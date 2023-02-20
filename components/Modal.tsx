6c82cc9e385ce5558750b84460be038ea1d82be4
jafar <jafar.fahimi12@gmail.com>
2023-02-20 08:02:52 +0430

    Modal.tsx XIcon added

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import MuiModal from "@mui/material/Modal";
 import { modalState } from "../atoms/modalAtoms";
 import { useRecoilState } from "recoil";
+import { XIcon } from "@heroicons/react/outline";
 
 export default function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
@@ -10,7 +11,14 @@ export default function Modal() {
     <>
       {/* open modal when showModal is true, when closing modal do handleClose */}
       <MuiModal open={showModal} onClose={handleClose}>
-        <>MuiModal</>
+        <>
+          <button onClick={handleClose} className="">
+            <XIcon width={45} height={45} />
+          </button>
+          <div>
+            
+          </div>
+        </>
       </MuiModal>
     </>
   );
