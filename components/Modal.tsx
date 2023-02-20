161bef687c6d66dee360aa21116a7efbc2ab1768
jafar <jafar.fahimi12@gmail.com>
2023-02-20 07:54:37 +0430

    Modal.tsx upgraded to <MuiModal>...

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import MuiModal from "@mui/material/Modal";
+import { modalState } from "../atoms/modalAtoms";
+import { useRecoilState } from "recoil";
 
 export default function Modal() {
-  return <div>Modal</div>;
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const handleClose = () => setShowModal(false);
+  return (
+    <>
+      {/* open modal when showModal is true, when closing modal do handleClose */}
+      <MuiModal open={showModal} onClose={handleClose}>
+        <>MuiModal</>
+      </MuiModal>
+    </>
+  );
 }
