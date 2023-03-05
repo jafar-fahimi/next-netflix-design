31c29a91ce1fb9be33554b45a7d5fb75d5d5041f
jafar <jafar.fahimi12@gmail.com>
2023-03-05 13:08:16 +0430

    utils/get-stripe.ts coded

---
+++ utils/get-stripe.ts
@@ -0,0 +1,15 @@
+// ./utils/get-stripejs.ts
+import { Stripe, loadStripe } from "@stripe/stripe-js";
+
+let stripePromise: Promise<Stripe | null>;
+const getStripe = () => {
+  // maybe async await!
+  if (!stripePromise) {
+    stripePromise = loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+    );
+  }
+  return stripePromise;
+};
+
+export default getStripe;
