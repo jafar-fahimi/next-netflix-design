05602937f5886a9dd40af72ec939f2bd74723837
jafar <jafar.fahimi12@gmail.com>
2023-03-31 11:32:13 +0430

    pages/cast/[castId] coded

---
+++ pages/cast/[castId].tsx
@@ -0,0 +1,41 @@
+import { motion } from "framer-motion";
+import Head from "next/head";
+import React from "react";
+import Footer from "../../components/Footer";
+import PersonFeed from "../../components/person/PersonFeed";
+import Header from "../../components/Header";
+import { NextPage } from "next";
+
+// router is changed in PersonMaping when user click on one person(PersonMaping)
+// it render PersonFeed(fetch castData & castPerson then passed them to KnownFor & PersonBanner)
+const CastPage: NextPage = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+    >
+      <Head>
+        <title>Netflix Design</title>
+      </Head>
+      <Header
+        links={[
+          "Netflix",
+          "Trending",
+          "Top",
+          "Action",
+          "Comedies",
+          "Scary",
+          "people",
+          "about",
+        ]}
+      />
+      <div className="bg-gradient-to-r from-gray-900">
+        <PersonFeed />
+        <Footer />
+      </div>
+    </motion.div>
+  );
+};
+
+export default CastPage;
