6e22bcf042d21fc242ef8fe786bb9fe607780978
jafar <jafar.fahimi12@gmail.com>
2023-04-01 19:34:46 +0430

    Row, MovieLine moved to Row

---
+++ components/Row/index.tsx
@@ -0,0 +1,124 @@
+import React, { useRef, useState } from "react";
+import { BiChevronLeftCircle, BiChevronRightCircle } from "react-icons/bi";
+import { Movie } from "../../typings";
+import MoviesLine from "./MoviesLine";
+
+type Props = {
+  movies?: Movie[];
+  title?: string;
+  isDetails?: boolean;
+  type: "movie" | "tv";
+  isSearch?: boolean;
+  isfavourite?: boolean;
+  likeMovies?: any;
+};
+
+const Row = ({
+  movies,
+  title,
+  isDetails = false,
+  type,
+  isSearch,
+  isfavourite,
+  likeMovies,
+}: Props) => {
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setIsMoved] = useState(false);
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true);
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div
+      className={
+        isfavourite || isSearch
+          ? `h-auto space-y-0.5 px-1 md:space-y-2`
+          : `h-40 space-y-0.5 px-4 md:space-y-2`
+      }
+    >
+      <h2
+        className={`${
+          isDetails
+            ? `w-56 cursor-pointer text-xl font-semibold text-[#e5e5e5] transition-colors duration-200 hover:text-white md:text-3xl`
+            : `w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition-colors duration-200 hover:text-white md:text-2xl`
+        }
+        ${
+          title
+            ? "mt-14 w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl"
+            : ""
+        }
+        `}
+      >
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        {!isSearch && (
+          <BiChevronLeftCircle
+            className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
+              !isMoved && "hidden"
+            }`}
+            onClick={() => handleClick("left")}
+          />
+        )}
+
+        {isfavourite ? (
+          <div
+            ref={rowRef}
+            className={
+              likeMovies?.length >= 4
+                ? `grid gap-x-10 gap-y-6 overflow-x-hidden sm:gap-x-14 md:gap-x-10 lg:gap-x-14`
+                : `flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2`
+            }
+          >
+            {likeMovies!.map((movie: any) => (
+              <MoviesLine
+                key={movie.id}
+                movie={movie.data()}
+                isDetails={isDetails}
+                type={type}
+                isfavourite={isfavourite}
+              />
+            ))}
+          </div>
+        ) : (
+          <div
+            ref={rowRef}
+            className={
+              isSearch && movies!.length >= 4
+                ? `grid gap-y-6 gap-x-24 overflow-x-hidden`
+                : `flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2`
+            }
+          >
+            {movies?.map((movie) => (
+              <MoviesLine
+                key={movie.id}
+                movie={movie}
+                isDetails={isDetails}
+                type={type}
+              />
+            ))}
+          </div>
+        )}
+
+        {!isSearch && (
+          <BiChevronRightCircle
+            className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+            onClick={() => handleClick("right")}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
